d6b9f69138b71d0f90c632010830da201b6d7c0b
arsh124218 <157267109+arsh124218@users.noreply.github.com>
2024-01-20 21:28:22 -0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,7 +12,7 @@ from Tools.Download import download
 
 
 my_father = "https://t.me/{}".format(Config.USER_NAME[1:])
-support = "https://telegram.dog/Ns_Bot_supporters"
+support = "@NITRO_LEECHERS"
 
 @Client.on_message(Filters.command(["start"]))
 async def start(c, m):
